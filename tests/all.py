f01befe40aff5b5ad36a8cb724a0ca38c5e2b4b0
Chris Forbes <chrisf@ijw.co.nz>
2014-03-15 21:45:10 +1300

    arb_texture_view: Add rendering test for layout consistency
    Signed-off-by: Chris Forbes <chrisf@ijw.co.nz>

--- tests/all.py
+++ tests/all.py
@@ -2034,6 +2034,7 @@ arb_texture_view['clear-into-view-2d-array'] = concurrent_test('arb_texture_view
 arb_texture_view['clear-into-view-layered'] = concurrent_test('arb_texture_view-clear-into-view-layered')
 arb_texture_view['copytexsubimage-layers'] = concurrent_test('arb_texture_view-copytexsubimage-layers')
 arb_texture_view['format-consistency-get'] = concurrent_test('arb_texture_view-format-consistency-get')
+arb_texture_view['format-consistency-render'] = concurrent_test('arb_texture_view-format-consistency-render')
 
 tdfx_texture_compression_fxt1 = Group()
 spec['3DFX_texture_compression_FXT1'] = tdfx_texture_compression_fxt1
